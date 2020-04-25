bb633cd82288e5b2fba790e26b5dae3aef30e12a
afa28 <andifahruddinakas@gmail.com>
2020-04-26 05:40:51 +0700

    Widget
    1. Ubah link widget statistik ke jenis statistik 4 (jenis kelamin)
    2. Perbaikan penulisan dan pemanggilan view

--- partials/bottom_content_right.php
+++ partials/bottom_content_right.php
@@ -5,21 +5,26 @@
 <!-- Tampilkan Widget -->
 <?php
 
-if($w_cos){
-	foreach($w_cos as $data){
-		if(($data["jenis_widget"] == 1) OR ($data["jenis_widget"] == 2)){
-			include("$this->theme_folder/$this->theme/widgets/".trim($data['isi']));
-		} else {
-			echo "
+if($w_cos):
+	foreach($w_cos as $data):
+		$widget = trim($data['isi']);
+		if(($data["jenis_widget"] == 1) OR ($data["jenis_widget"] == 2)):
+			$cek = explode('/', $widget);
+			if($cek[1] !== NULL):
+				$cek = $cek[1];
+			else:
+				$cek = $cek[0];
+			endif;
+			include($this->theme_folder.'/'.$this->theme.'/widgets/'.$cek);
+		else: ?>
 			<div class=\"single_bottom_rightbar\">
-			    <h2><i class='fa fa-folder-open'></i> ".$data["judul"]."</h2>
+			    <h2><i class='fa fa-folder-open'></i> <?=$data["judul"]?></h2>
 				<div class=\"box-body\">
-				".html_entity_decode($data['isi'])."
+					<?=html_entity_decode($data['isi'])?>
 				</div>
 			</div>
-			";
-		}
-	}
-}
+		<?php endif;
+	endforeach;
+endif;
 ?>
 </div>
