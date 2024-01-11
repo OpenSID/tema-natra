a608d44fce4568ef9756bc33333865627af91c2f
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-01-11 12:04:52 +0800

    v2401.0.0

--- partials/idm/index.php
+++ partials/idm/index.php
@@ -1,16 +1,16 @@
-<link rel="stylesheet" href="<?= base_url()?>assets/css/AdminLTE.css" />
+<link rel="stylesheet" href="<?= asset('css/AdminLTE.css') ?>" />
 <!-- Font Awesome -->
 <link rel="stylesheet" href="<?= asset('bootstrap/css/font-awesome.min.css') ?>">
 <!-- Ionicons -->
 <link rel="stylesheet" href="<?= asset('bootstrap/css/ionicons.min.css') ?>">
 <?php if (is_file($this->theme_folder . '/' . $this->theme . '/css/first.css')): ?>
-	<link rel="stylesheet" href="<?= base_url() . $this->theme_folder . '/' . $this->theme . '/css/first.css' ?>" />
+	<link rel="stylesheet" href="<?= base_url("{$this->theme_folder}/{$this->theme}/css/first.css") ?>" />
 <?php endif; ?>
 <?php if (is_file($this->theme_folder . '/' . $this->theme . '/assets/css/desa-web.css')): ?>
-	<link type='text/css' href="<?= base_url() . $this->theme_folder . '/' . $this->theme . '/assets/css/desa-web.css' ?>" rel='stylesheet' />
+	<link type='text/css' href="<?= base_url("{$this->theme_folder}/{$this->theme}/assets/css/desa-web.css") ?>" rel='stylesheet' />
 <?php endif; ?>
 <?php if (is_file('desa/css/' . $this->theme . '/desa-web.css')): ?>
-	<link type='text/css' href="<?= base_url()?>desa/css/<?= $this->theme ?>/desa-web.css" rel='Stylesheet' />
+	<link type='text/css' href="<?= base_url("desa/css/{$this->theme}/desa-web.css") ?>" rel='Stylesheet' />
 <?php endif; ?>
 <style>
     .small-box .icon {
@@ -169,7 +169,7 @@
 												<td><?= $data->KAB ?></td>
 												<td><?= $data->DESA ?></td>
 												<td><?= $data->CSR ?></td>
-												<td><?= $data->SKOR[INDIKATOR['IKS 2020']] ?></td>
+												<td><?= $data->LAINNYA ?></td>
 											</tr>
 										<?php endforeach; ?>
 									</tbody>
