c966843588fda946ed9b72c88c7a1e5ed5bc7721
Habibie <habibie.mahbub@gmail.com>
2024-02-26 18:07:25 +0700

    tambah kondisi jika BELUM MENGISI

--- widgets/statistik.php
+++ widgets/statistik.php
@@ -28,11 +28,11 @@
 						xAxis: {
 							categories:
 							[
-							<?php foreach($stat_widget as $data): ?>
-								<?php if ($data['jumlah'] != "-" AND $data['nama']!= "JUMLAH"): ?>
-									['<?= $data['jumlah']?> <br> <?= $data['nama']?>'],
-								<?php endif; ?>
-							<?php endforeach; ?>
+								<?php foreach($stat_widget as $data): ?>
+									<?php if ($data['jumlah'] != "-" && $data['nama']!= "JUMLAH" && $data['nama'] != "BELUM MENGISI"): ?>
+										['<?= $data['jumlah']?> <br> <?= $data['nama']?>'],
+									<?php endif; ?>
+								<?php endforeach; ?>
 							]
 						},
 						legend: {
@@ -50,12 +50,13 @@
 						series: [{
 							type: 'column',
 							name: 'Populasi',
-							data: [
-							<?php foreach ($stat_widget as $data): ?>
-								<?php if ($data['jumlah'] != "-" AND $data['nama']!= "JUMLAH"): ?>
-									['<?= $data['nama']?>',<?= $data['jumlah']?>],
-								<?php endif; ?>
-							<?php endforeach; ?>
+							data: 
+							[
+								<?php foreach($stat_widget as $data): ?>
+									<?php if ($data['jumlah'] != "-" && $data['nama']!= "JUMLAH" && $data['nama'] != "BELUM MENGISI"): ?>
+										['<?= $data['jumlah']?> <br> <?= $data['nama']?>'],
+									<?php endif; ?>
+								<?php endforeach; ?>
 							]
 						}]
 					});
